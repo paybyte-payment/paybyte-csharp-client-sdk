52815569cb2cdaf19f2b8c5365ab1ca1418c299e
izzycoder18 <andrea@setgetgo.com>
2018-04-16 19:29:56 +0100

    ctor added

--- SetGetGo.Sdk/SetGetGo.cs
+++ SetGetGo.Sdk/SetGetGo.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SetGetGo.Sdk
+﻿namespace SetGetGo.Sdk
 {
     public class SetGetGo
     {
+        public SetGetGo(bool isTestNet)
+        {
+
+        }
+
+
     }
 }
